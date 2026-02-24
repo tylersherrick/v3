45478782a545f826c0e1e752a006ba9b527ae279
Tyler Sherrick <tyler.sherrick@gmail.com>
2026-02-24 14:48:38 -0600

    updated the game records and gamestat components with some test base data.

--- src/components/GameRecords.jsx
+++ src/components/GameRecords.jsx
@@ -1,5 +1,28 @@
 const GameRecords = () => {
-  return <div>Records</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <h5>Team Records</h5>
+      </div>
+
+      <div style={{ marginTop: "15px" }}></div>
+
+      <div className="row">
+        <h7>Full date and time</h7>
+      </div>
+
+      <div className="row">
+        <h7>Coverage</h7>
+      </div>
+
+      <div style={{ marginTop: "35px" }}></div>
+
+      <div className="row">
+        <h7>Venue and Location</h7>
+      </div>
+
+    </div>
+  );
 };
 
 export default GameRecords;
--- src/components/GameStats.jsx
+++ src/components/GameStats.jsx
@@ -1,5 +1,28 @@
 const GameStats = () => {
-  return <div>Stats</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <h5>Team Stats</h5>
+      </div>
+
+      <div style={{ marginTop: "15px" }}></div>
+
+      <div className="row">
+        <h7>Full date and time</h7>
+      </div>
+
+      <div className="row">
+        <h7>Coverage</h7>
+      </div>
+
+      <div style={{ marginTop: "35px" }}></div>
+
+      <div className="row">
+        <h7>Venue and Location</h7>
+      </div>
+
+    </div>
+  );
 };
 
 export default GameStats;
