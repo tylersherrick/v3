e093e4fa3832c0ea4e18e32ebec120caefbbb482
Tyler Sherrick <tyler.sherrick@gmail.com>
2026-02-25 10:05:09 -0600

    updated GameInfo so that the information is being pulled into the gameinfo tab when active. will update records and stats after.

--- src/components/GameInfo.jsx
+++ src/components/GameInfo.jsx
@@ -1,4 +1,26 @@
-const GameInfo = () => {
+const GameInfo = ({game}) => {
+
+  let gameTime = game.status.type.detail;
+  let venue = game.competitions[0].venue.fullName || "";
+  let city = game.competitions[0].venue.address.city || "";
+  let state = game.competitions[0].venue.address.state || "";
+  let coverage = game?.competitions?.[0]?.geoBroadcasts?.[0]?.media?.shortName || "";
+  const comp = game?.competitions?.[0];
+  const gameTimeLocal = comp?.date
+    ? new Date(comp.date).toLocaleString(undefined, {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "2-digit",
+        hour12: true,
+      })
+    : "";
+
+  if(venue != "") {
+    venue = venue + " - ";
+  }
+  
   return (
     <div className="container">
       <div className="row">
@@ -8,17 +30,17 @@ const GameInfo = () => {
       <div style={{ marginTop: "15px" }}></div>
 
       <div className="row">
-        <h7>Full date and time</h7>
+        <h7>{gameTimeLocal}</h7>
       </div>
 
       <div className="row">
-        <h7>Coverage</h7>
+        <h7>{coverage}</h7>
       </div>
 
       <div style={{ marginTop: "35px" }}></div>
 
       <div className="row">
-        <h7>Venue and Location</h7>
+        <h7>{venue} {city + ", " + state}</h7>
       </div>
 
     </div>
